0d11b80f81c382d83d7f770136e23e38eb1ec0c4
daniel-enere <daniel.enere@gmail.com>
2018-06-06 19:58:36 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const debug = require('debug')('server');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const post = require('./routes/api/post');
+
+const app = express();
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to mongoDB
+mongoose
+  .connect(db)
+  .then(() => debug('**** Connected to MongoDB ****'))
+  .catch(err => debug(err));
+
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/post', post);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  debug(`Server up on ${port}`);
+})
