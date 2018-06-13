ce50479caeaeb77ea3fc2286b3b7d299e6f60e3c
daniel-enere <daniel.enere@gmail.com>
2018-06-13 08:21:00 -0500

    added redux and created reducers and actions dir and store file

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -34,8 +34,8 @@ class Login extends Component {
           <div className="row">
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Log In</h1>
-              <p className="lead text-center">Sign in to your DevConnector account</p>
-              <form onSubmit={this.onSubmit}>
+              <p className="lead text-center">Sign in to your DevConnect account</p>
+              <form noValidate onSubmit={this.onSubmit}>
                 <div className="form-group">
                   <input type="email"
                     className="form-control form-control-lg"
