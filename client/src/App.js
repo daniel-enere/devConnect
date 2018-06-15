695c420aaabe977feaf656d213744202ba63d623
daniel-enere <daniel.enere@gmail.com>
2018-06-15 13:43:56 -0500

    initial profile state

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,8 @@ import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
+import { clearCurrentProfile } from './actions/profileActions';
 import store from './store';
 import './App.css';
 
@@ -30,7 +32,7 @@ if(localStorage.jwtToken) {
 
     store.dispatch(logoutUser()); //Logout user
 
-    // TODO: Clear current profile
+    store.dispatch(clearCurrentProfile()); // Clear current profile
 
     // Redirect to login
     window.location.href = '/login';
@@ -49,6 +51,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={ Register } />
               <Route exact path="/login" component={ Login } />
+              <Route exact path="/dashboard" component={Dashboard} />
             </div>
             <Footer/>
           </div>
