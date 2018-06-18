405c65f9c4f1fd4647deec2af55b3f4cac8b1286
daniel-enere <daniel.enere@gmail.com>
2018-06-18 18:08:19 -0500

    added create profile route and functionality

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Dashboard from './components/dashboard/Dashboard';
 import { clearCurrentProfile } from './actions/profileActions';
 import store from './store';
 import PrivateRoute from './components/common/PrivateRoute';
+import CreateProfile from './components/create-profile/CreateProfile';
 import './App.css';
 
 // Check for token
@@ -54,6 +55,9 @@ class App extends Component {
               <Route exact path="/login" component={ Login } />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/create-profile" component={CreateProfile} />
               </Switch>
             </div>
             <Footer/>
