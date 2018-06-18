2ed3d2d21bae086ba7d3c8d44e55eb1e9b3764d8
daniel-enere <daniel.enere@gmail.com>
2018-06-18 15:32:51 -0500

    Profile and Dashboard routing added

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import jwt_decode from 'jwt-decode';
 
@@ -13,6 +13,7 @@ import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
 import { clearCurrentProfile } from './actions/profileActions';
 import store from './store';
+import PrivateRoute from './components/common/PrivateRoute';
 import './App.css';
 
 // Check for token
@@ -51,7 +52,9 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={ Register } />
               <Route exact path="/login" component={ Login } />
-              <Route exact path="/dashboard" component={Dashboard} />
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
             </div>
             <Footer/>
           </div>
---
+++ client/src/components/create-profile/CreateProfile.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class CreateProfile extends Component {
+  render() {
+
+  }
+}
+
+export default connect(null)(CreateProfile);
