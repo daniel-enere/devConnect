2bebe31f7de7fd4f8a5b965a10926bf591c4f545
daniel-enere <daniel.enere@gmail.com>
2018-06-20 12:44:46 -0500

    added github api and not found for profile route

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -18,6 +18,12 @@ class Profile extends Component {
     }
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.profile.profile === null && this.props.profile.loading) {
+      this.props.history.push('/not-found');
+    }
+  }
+
   render() {
     const { profile, loading } = this.props.profile;
     let profileContent;
@@ -37,8 +43,11 @@ class Profile extends Component {
           </div>
           <ProfileHeader profile={profile} />
           <ProfileAbout profile={profile}/>
-          <ProfileCreds education={profile.education} experience={profile.experience} />
-          <ProfileGithub />
+          <ProfileCreds
+            education={profile.education}
+            experience={profile.experience}
+          />
+          {profile.githubusername ? (<ProfileGithub username={profile.githubusername} />) : null }
         </div>
       )
     }
