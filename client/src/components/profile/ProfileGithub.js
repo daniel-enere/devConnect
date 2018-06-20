9fafda85660073570be8ec70e6c13b34ad1e60fd
daniel-enere <daniel.enere@gmail.com>
2018-06-20 16:53:07 -0500

    bug fix

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -22,7 +22,7 @@ class ProfileGithub extends Component {
       .then(res => res.json())
       .then(data => {
         if (this.refs.myRef) {
-          this.setState({repos: data});  
+          this.setState({repos: data});
         }
       })
       .catch(err => console.log(err))
