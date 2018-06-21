ed67f89e0474fe66b1f4937516ce4a19b747bb51
daniel-enere <daniel.enere@gmail.com>
2018-06-21 06:59:10 -0500

    deployment prep

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const debug = require('debug')('server');
+const path = require('path');
 
 
 const users = require('./routes/api/users');
@@ -34,11 +35,25 @@ app.use('/home', (req, res) => {
   res.json({home:"This will be the home/landing page"});
 });
 
+// Use Routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/post', post);
 
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+
+  // set static folder
+  app.use(express.static('client/build'));
+
+  // route
+  app.get('*', (req, res ) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => {
   debug(`Server up on ${port}`);
 })
